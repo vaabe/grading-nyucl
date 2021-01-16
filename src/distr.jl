051e65ef42701cf55da66868bbd829cfae66eda9
vaabe <accounts@vinzaabe.com>
2021-01-16 16:48:03 -0500

    change jl line str

--- src/distr.jl
+++ src/distr.jl
@@ -5,8 +5,5 @@ gradefile = readdlm(string(ARGS[1], "/grades.csv"), ',')
 grades = gradefile[2:size(gradefile)[1], 5]
 grades = convert(Array{Float64}, grades)
 
-distrib = histogram(grades, 
-					nbins = 5, 
-					closed = :right)
-
+distrib = histogram(grades, nbins = 5, closed = :right)
 println(distrib)
